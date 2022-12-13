27b641204b81d92de7e22fe52710e16724395d34
Kabelo <kabzastiffla@gmail.com>
2022-12-13 10:42:38 -0800

    abc

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * return: Always 0 (success)
+ */
+
+void print_alphabet(void);
+{
+	char c = 'a';
+
+	while
+	(c <= 'z');
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
