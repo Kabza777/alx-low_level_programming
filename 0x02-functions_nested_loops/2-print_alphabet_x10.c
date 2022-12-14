81c42dde18919489bcd8120e22752efa7b98283d
Kabelo <kabzastiffla@gmail.com>
2022-12-13 17:45:43 -0800

    Readme

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_alphabet - Entry point
+ *
+ * return: Always 0 (success)
+ */
+
+void print_alphabet
+{                                                                                                                               
+
+	        char c = 'a';                                                                                                           
+
+		                                                                                                                                
+
+		        while                                                                                                                   
+
+				        (c <= 'z');                                                                                                             
+
+			        {                                                                                                                       
+
+					                _putchar(c);                                                                                                    
+
+							                c++;                                                                                                            
+
+									        }                                                                                                                       
+
+				        _putchar('\n');                                                                                                         
+
+} 
