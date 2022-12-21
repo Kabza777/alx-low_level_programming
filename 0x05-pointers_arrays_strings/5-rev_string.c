1c6eb0f7d535a2e51f9480885cda7bf23b3c43c3
Kabelo <kabzastiffla@gmail.com>
2022-12-20 19:55:27 -0800

    crazy terminal

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * rev_string - a function that reverses a string.
+ * @s: input
+ */
+void rev_string(char *s)
+{
+	char rv = s[0];
+	int c = 0;
+	int i;
+
+	while (s[c] != '\0')
+		s++;
+	for (i = 0; i < c; i++)
+	{
+		c--;
+		rv = s[i];
+		s[i] = s[c];
+		s[c] = rv;
+	}
+}
